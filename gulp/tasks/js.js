f44aa5a2586c72fbbc36297e57918aa6d4209109
John Doe <c35292@yandex.ru>
2022-02-15 10:08:27 +0300

    Sourcemaps для js

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,15 +1,19 @@
 const { src, dest, series } = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
 const plumber = require('gulp-plumber');
+const gulpIf = require('gulp-if');
 const babel = require('gulp-babel');
 const { server } = require('./server');
 const config = require('../config');
 
 function jsCore () {
   return src(`${config.src.js}/*.js`)
+    .pipe(gulpIf(!config.production, sourcemaps.init()))
     .pipe(plumber({
       errorHandler: config.errorHandler
     }))
     .pipe(babel())
+    .pipe(gulpIf(!config.production, sourcemaps.write('./')))
     .pipe(dest(config.dest.js))
     .pipe(server.reload({stream: true}));
 }
