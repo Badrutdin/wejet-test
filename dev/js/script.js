4bc2ab89f500492f5ad3b5241b57036c0e81362b
Badrutdin <ataev.b@introvert.bz>
2022-02-16 02:56:33 +0300

    верстка формы и JS функционал

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,5 +1,6 @@
 const actions = function () {
-  $('.popup').magnificPopup();
+  $('.popup').magnificPopup({});
+  $('.owl-carousel').owlCarousel();
   $(document).on('click', '[data-scroll]', function (e) {
     e.preventDefault();
     let target = $(this.getAttribute('href')).get(0);
@@ -9,6 +10,35 @@ const actions = function () {
       }, 200);
     }
   })
+  $(document).on('focus', '.animated-input__input', function () {
+    $(this).closest('.animated-input').find('.animated-input__label').addClass('animated-input__label_focus')
+  });
+  $(document).on('blur', '.animated-input__input', function () {
+    $(this).closest('.animated-input').find('.animated-input__label').removeClass('animated-input__label_focus')
+  });
+  $(document).on('input', '.animated-input__input', function () {
+    if ($(this).val() !== '') {
+      $(this).addClass('animated-input__input_changed')
+      $(this).closest('.animated-input').find('.animated-input__label').addClass('animated-input__label_changed')
+    } else {
+      $(this).removeClass('animated-input__input_changed')
+      $(this).closest('.animated-input').find('.animated-input__label').removeClass('animated-input__label_changed')
+    }
+  });
+  // просто добавил поэксперементировать
+  $(document).on('focus', '[type="tel"]',function(){
+    if ($(this).val() === ''){
+    $(this).val('+7')
+    $(this).trigger('input')
+    }
+  })
+  $(document).on('blur', '[type="tel"]',function(){
+    if ($(this).val() === '+7'){
+      $(this).val('')
+      $(this).trigger('input')
+    }
+  })
+
 }
 
 $(document).ready(function () {
